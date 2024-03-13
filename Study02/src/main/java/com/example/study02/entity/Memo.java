07542196c465a12d11da52e31dee7f9f3788272c
박찬영 <144075327+CD3660@users.noreply.github.com>
2024-03-13 23:34:43 +0900

    240313

---
+++ Study02/src/main/java/com/example/study02/entity/Memo.java
@@ -0,0 +1,20 @@
+package com.example.study02.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "Memo")
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Memo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long mno;
+
+    @Column(length = 200, nullable = false)
+    private String memoText;
+}
---
+++ Study02/src/test/java/com/example/study02/repository/MemoRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.example.study02.repository;
+
+import com.example.study02.entity.Memo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class MemoRepositoryTest {
+
+    @Autowired
+    MemoRepository memoRepository;
+
+    @Test
+    public void testClass() {
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Memo memo = Memo.builder().MemoText("sample..." + i).build();
+            memoRepository.save(memo);
+        });
+    }
+
+    @Test
+    public void testSelect() {
+        Long mno = 100L;
+
+        Optional<Memo> result = memoRepository.findById(mno);
+
+        System.out.println("==============================");
+
+        if (result.isPresent()) {
+            Memo memo = result.get();
+            System.out.println(memo);
+        }
+
+    }
+
+    @Test
+    public void testPageDefault() {
+        Sort sort1 = Sort.by("mno").descending();
+        Sort sort2 = Sort.by("memoText").ascending();
+        Sort sortAll = sort1.and(sort2);
+        Pageable pageable = PageRequest.of(0,10, sort); // 앞쪽이 현재 페이지 정보, 뒤는 페이지 당 개수
+
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+
+        System.out.println("-----------------------------");
+        System.out.println("TotalPage: "+result.getTotalPages());  // 총 페이지 수
+        System.out.println("TotalCount: "+result.getTotalElements());  // 전체 레코드 개수
+        System.out.println("Page Number: "+result.getNumber()); // 현재 페이지 번호
+        System.out.println("Page Size: "+result.getSize());  //페이지 당 데이터 개수
+        System.out.println("Has Next Page: "+result.hasNext());  // 다음 페이지 존재 여부
+        System.out.println("first page: "+result.isFirst());  // 이전 페이지 존재 여부 == 0페이지인지
+        System.out.println("-----------------------------");
+        for (Memo memo:result.getContent()){
+            System.out.println(memo);
+        }
+    }
+
+
+}
